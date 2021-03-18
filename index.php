2f60186092e787cdb5123ba1b65c105228232213
jannajothomas <jannajothomas@gmail.com>
2021-03-18 16:08:28 -0400

    Add post

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
- 
+
+	if (isset($_POST["checkbox"])) { 
+		$post_data = file_get_contents('php://input'); 
+		echo "<div> POST BODY <br>".$post_data."</div>";		 
+	} 
+
+
+
 $xml = new DOMDocument();
 $xml->load('routeList.xml');
  
@@ -10,3 +17,4 @@ $proc = new XSLTProcessor();
 $proc->importStyleSheet($xsl);
  
 echo $proc->transformToXML($xml);
+?> 
