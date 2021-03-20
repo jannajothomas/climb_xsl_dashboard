e85f1a2949749bee1da028a27442d31ff7457bd0
jannajothomas <jannajothomas@gmail.com>
2021-03-20 08:08:03 -0400

    Post contains route name

--- index.php
+++ index.php
@@ -30,23 +30,17 @@ session_start();
     $csvConvert=newDOMDocument();
     $csvConvert->load('csvconvert.xsl');
     
-    $csvConvertProcessor = new XSLTProcessor();
+    //$csvConvertProcessor = new XSLTProcessor();
     $csvConvertProcessor->importStyleSheet($csvConvert);
 
-    echo $csvConvertProcessor->transformToXML($ticksCSV);
+    //echo $csvConvertProcessor->transformToXML($ticksCSV);
 
-    $ticksCSVFile = fopen("ticks.csv","w");
-    fwrite($ticksCSVFile, $csvConvertProcessor);
-    fclose($ticksCSVFile);
+    //$ticksCSVFile = fopen("ticks.csv","w");
+    //fwrite($ticksCSVFile, $csvConvertProcessor);
+    //fclose($ticksCSVFile);
 
 
     //pull routes and save as routes.csv
-$ticksCSV = file_get_contents("https://www.mountainproject.com/user/200907314/janna-thomas/tick-export");
-
- $domTicksCSV = new DOMDocument();
- $domTicksCSV->load($ticksCSV);
-
-echo "No errors produced"
         //https://www.mountainproject.com/route-finder-export?type=rock&diffMinrock=1000&diffMinboulder=20000&diffMinaid=70000&diffMinice=30000&diffMinmixed=50000&diffMaxrock=12400&diffMaxboulder=20050&diffMaxaid=75260&diffMaxice=38500&diffMaxmixed=60000&is_sport_climb=1&stars=0&pitches=0&selectedIds=105867829
     //transform ticks.csv to ticks.xml   prefer php, but can use script
     //transform routes.csc to routes.xml  prefer php, but can use script
